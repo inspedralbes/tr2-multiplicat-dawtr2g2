1c628dc9b94c1059450894cd72a0a8711ceeed20
= <=>
2023-11-30 09:59:35 +0100

    reoutes fixed

--- vue-node/src/router/index.js
+++ vue-node/src/router/index.js
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import Game from '../components/battlemathGame.vue'
+import Battle from '../views/BattleView.vue'
+import Landing from '../views/LandingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      // path: '/',
-      // name: 'home',
-      // component: HomeView
+      path: '/',
+      name: 'landing',
+      component: Landing
     },
     {
-      // path: '/about',
-      // name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
+      path: '/game',
+      name: 'game',
+      component: Game
+    },
+    {
+      path: '/battle',
+      name: 'battle',
+      component: Battle
     }
   ]
 })
