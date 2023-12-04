b7487c9d58b4aeed7d343c4013e405c1adbe1ef4
Pedro Garcia <a20pedgarguz@inspedralbes.cat>
2023-12-04 09:53:36 +0100

    Fixing Index

--- vue-node/src/router/index.js
+++ vue-node/src/router/index.js
@@ -5,6 +5,7 @@ import partida from '../components/partida.vue'
 import Battle from '../views/BattleView.vue'
 import Landing from '../views/LandingView.vue'
 import Login from '../views/Login.vue'
+import Battlemath from '../views/battlemathGame.vue'
 import Register from '../views/RegisterView.vue'
 
 const router = createRouter({
@@ -39,11 +40,6 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: Register
-    },
-    {
-      path: '/game',
-      name: 'game',
-      component: Game
     },
     {
       path: '/battle',
