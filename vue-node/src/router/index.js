5394f6a5fdf1aeca92bd42b13421ef67c0d18019
Pedro Garcia <a20pedgarguz@inspedralbes.cat>
2023-12-04 10:17:03 +0100

    fixed bug landing

--- vue-node/src/router/index.js
+++ vue-node/src/router/index.js
@@ -12,11 +12,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: test
-    },
-    {
       path: '/lleno',
       name: 'lleno',
       component: lleno
