bfc4c865d34447709a5305005ac6f89bebb93d82
= <=>
2023-12-05 13:06:23 +0100

    llistat sales

--- vue-node/src/router/index.js
+++ vue-node/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Battle from '../views/BattleView.vue'
 import Landing from '../views/LandingView.vue'
 import Battlemath from '../views/battlemathGame.vue'
+import Rooms from '../views/RoomsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,12 @@ const router = createRouter({
       path: '/game',
       name: 'battlemath',
       component: Battlemath
-    }
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: Rooms
+    },
   ]
 })
 
---
+++ vue-node/src/views/RoomsView.vue
@@ -0,0 +1,153 @@
+<template>
+    <div class="rooms_container">
+        <div class="box">
+            <div class="options">
+                <button class="option">LISTA PARTIDAS</button>
+                <button class="option">CREAR PARTIDAS</button>
+            </div>
+            <div class="rooms">
+                <div class="room">
+                    <h2 class="title">Partida Clase #1</h2>
+                    <h3 class="user">Julian</h3>
+                    <h4 class="capacity">1/2</h4>
+                    <button class="join-btn">Unirse</button>
+                </div>
+
+                <div class="room">
+                    <h2 class="title">Hikkimikki</h2>
+                    <h3 class="user">Deividson</h3>
+                    <h4 class="capacity">2/2</h4>
+                    <p>Completa</p>
+                </div>
+
+                <div class="room">
+                    <h2 class="title">Que plasta</h2>
+                    <h3 class="user">Yoyo</h3>
+                    <h4 class="capacity">2/2</h4>
+                    <p>Completa</p>
+                </div>
+
+                <div class="room">
+                    <h2 class="title">Partida Clase #2</h2>
+                    <h3 class="user">Dabid Visval</h3>
+                    <h4 class="capacity">1/2</h4>
+                    <button class="join-btn">Unirse</button>
+                </div>
+
+                <div class="room">
+                    <h2 class="title">La Ganga</h2>
+                    <h3 class="user">ElBryan</h3>
+                    <h4 class="capacity">1/2</h4>
+                    <button class="join-btn">Unirse</button>
+                </div>
+
+                <div class="room">
+                    <h2 class="title">Me gusta la perassss</h2>
+                    <h3 class="user">MrPera</h3>
+                    <h4 class="capacity">2/2</h4>
+                    <p>Completa</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.rooms_container {
+    width: 100%;
+    height: 100vh;
+    background-image: url(../../public/img/pixelArt-background.jpeg);
+    background-size: cover;
+    display: flex;
+    align-items: center;
+}
+
+.box{
+    position: relative;
+    width: 70%;
+    height: 35%;
+    border: 4px solid black;
+    margin: 0 auto;
+    border-radius: 20px;
+    
+}
+.options{
+    position: absolute;
+    top: -30px;
+    left: 40px;
+    display: flex;
+
+}
+.option{
+    background-color: #282828;
+    text-transform: uppercase;
+    color: white;
+    font-weight: bold;
+    padding: 15px 10px;
+    border: 2px solid #E58D08;
+    border-radius: 15px;
+}
+
+.rooms{
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+    height: 270px;
+    overflow-y: scroll;
+}
+.room{
+    width: 90%;
+    min-height: 58px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: center;
+    justify-items: center;
+    margin: 0 auto;
+    background-color: #282828;
+    color: white;
+    border-radius: 10px;
+    border: 2px solid #E58D08;
+}
+.title{
+    text-align: center;
+}
+
+.join-btn{
+    padding: 5px 10px;
+    background-color: #A1FB4F;
+    color: black;
+    border-radius: 10px;
+    transition: all 0.3s ease;  
+}
+.join-btn:hover{
+    background-color: #E58D08;
+    color: white;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 100px;
+  margin-right: 5px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #E58D08;
+  border-radius: 100px;
+  margin-right: 5px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #282828; 
+}
+
+</style>
