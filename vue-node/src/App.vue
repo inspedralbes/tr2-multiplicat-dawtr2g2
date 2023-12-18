d8570b7f046ccf104ac5c8dc5d34b530cf619a68
= <=>
2023-12-18 11:42:32 +0100

    Add background music to the app

--- vue-node/src/App.vue
+++ vue-node/src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+
+const playMusic = () => {
+  const audio = new Audio("/audio/coral-chorus.mp3");
+  audio.loop = true;
+  audio.play();
+};
+
+onMounted(() => {
+  playMusic();
+});
 </script>
 
 <template>
   <RouterView />
 </template>
-
