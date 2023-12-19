8a223df885c8171cf8d29195a48365c219e14f8a
a22rauespgom <a22rauespgom@inspedralbes.cat>
2023-12-19 19:11:48 +0100

    Update lobbyMap.tmx, lobbyMap.json, char_select.vue, music.vue, partida.vue

--- vue-node/src/components/partida.vue
+++ vue-node/src/components/partida.vue
@@ -3,6 +3,8 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <div class="container__game">
+        <button class="nes-btn sortir" @click="sortir">Surt</button>
+
         <div class="game">
             <header class="header">
                 <div class="player player1">
@@ -45,8 +47,9 @@
                 </div>
                 <div class="question__container">
                     <h2 class="tematica">GEOMETRIA</h2>
-                    <H3 class="question" >{{ quest.pregunta }}</H3>
-                    <h4 v-if="showEst" :class="{ correct: est === 'Correcte', incorrect: est === 'Incorrecte' }">{{ est }}</h4>
+                    <H3 class="question">{{ quest.pregunta }}</H3>
+                    <h4 v-if="showEst" :class="{ correct: est === 'Correcte', incorrect: est === 'Incorrecte' }">{{ est }}
+                    </h4>
                 </div>
                 <div class="character" v-if="room.players.length == 2" style="transform: scaleX(-1);">
                     <img :src="`/characters/${room.players[1].skin}_fight.png`" alt="">
@@ -163,7 +166,7 @@ export default {
             this.quest = '';
             this.est = 'Correcte'
             this.showEstForThreeSeconds();
-            
+
         });
 
         socket.on('incorrect', () => {
@@ -173,9 +176,9 @@ export default {
         });
 
         watch(() => this.numQuest, newVal => {
-        if (newVal == 0) {
-            this.numQuest = 10;
-        }
+            if (newVal == 0) {
+                this.numQuest = 10;
+            }
         });
     },
     methods: {
@@ -198,6 +201,9 @@ export default {
                 this.showEst = false;
             }, 3000);
         },
+        sortir() {
+            this.$router.push("/rooms");
+        }
 
     },
 
@@ -206,14 +212,15 @@ export default {
   
 <style scoped>
 .correct {
-  color: green;
-  font-size: 30px;
+    color: green;
+    font-size: 30px;
 }
 
 .incorrect {
-  color: red;
-  font-size: 30px;
+    color: red;
+    font-size: 30px;
 }
+
 * {
     margin: 0;
     padding: 0;
@@ -558,6 +565,35 @@ main {
 
 .blue {
     background-color: #5ad5db;
+}
+
+.sortir {
+    border-image-repeat: stretch !important;
+    background-color: #ffad5d !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 3%;
+    right: 5%;
+    width: 6vw;
+    height: 3vh;
+}
+
+.sortir::after {
+    box-shadow: inset -4px -4px #e46d3a !important;
+}
+
+.sortir:hover {
+    background-color: #ec9e50 !important;
+}
+
+.sortir:hover::after {
+    box-shadow: inset -6px -6px #e46d3a !important;
+}
+
+.nes-btn:active:not(.is-disabled)::after {
+    box-shadow: inset 4px 4px #e46d3a !important;
 }
 </style>
   
