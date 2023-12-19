0396a668cf35dff8e34fac15d0a5c804e4ce8f25
a22ivbs <a22ivabarsan@inspedralbes.cat>
2023-12-19 12:07:56 +0100

    Add music component to App.vue and LandingView.vue

--- vue-node/src/App.vue
+++ vue-node/src/App.vue
@@ -1,70 +1,14 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import music from "./components/music.vue";
 
-const audio = new Audio("/audio/coral-chorus.mp3");
-audio.loop = true;
-
-const isMuted = ref(false);
-
-const toggleMute = () => {
-    isMuted.value = !isMuted.value;
-    audio.muted = isMuted.value;
-};
-
-onMounted(() => {
-    audio.play();
-});
 </script>
 
 <template>
-    <button class="nes-btn" @click="toggleMute">
-        <img
-            :src="
-                isMuted
-                    ? '/public/img/mute-sound.png'
-                    : '/public/img/unmute-sound.png'
-            "
-            alt=""
-        />
-    </button>
+    <music />
     <RouterView />
 </template>
 
-<style>
-button {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    z-index: 1;
-    width: 50px;
-    height: auto;
-}
 
-img {
-    width: 100%;
-    height: 100%;
-}
 
-button {
-    border-image-repeat: stretch !important;
-    background-color: #ffad5d !important;
-    margin-top: 25px;
-}
-
-button::after {
-    box-shadow: inset -4px -4px #e46d3a !important;
-}
-
-button:hover {
-    background-color: #ec9e50 !important;
-}
-
-button:hover::after {
-    box-shadow: inset -6px -6px #e46d3a !important;
-}
-
-.nes-btn:active:not(.is-disabled)::after {
-    box-shadow: inset 4px 4px #e46d3a !important;
-}
-</style>
