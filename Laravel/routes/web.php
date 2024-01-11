f13d2abdd485f2eb0c3b3a931b4bb82594ef63f4
= <=>
2024-01-11 09:37:30 +0100

    Add login functionality and secure routes

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PreguntaController;
 use App\Http\Controllers\RespostaController;
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,22 +15,30 @@ use App\Http\Controllers\RespostaController;
 |
 */
 
+// Rutas de inicio de sesión y cierre de sesión
+Route::get('/', function () {
+    return view('login');
+})->name('login');
 
-//preguntes
-Route::get('/', [PreguntaController::class, 'adminIndex'])->name('preguntes');
-Route::get('/preguntes/afegir', function () { return view('preguntes.afegir');})->name('view-afegir-pregunta');
-Route::post('/preguntes/afegir', [PreguntaController::class, 'adminStore'])->name('afegir-pregunta');
-Route::get('/preguntes/modificar/{id}', [PreguntaController::class, 'adminShow'])->name('view-modificar-pregunta');
-Route::patch('/preguntes/modificar/{id}', [PreguntaController::class, 'adminUpdate'])->name('modificar-pregunta');
-Route::delete('/preguntes/{id}', [PreguntaController::class, 'adminDelete'])->name('eliminar-pregunta');
+Route::post('/login', [LoginController::class, 'login'])->name('loginAdmin');
 
 
 
-//respostes
-Route::get('/respostes', [RespostaController::class, 'adminIndex'])->name('respostes');
-Route::get('/respostes/afegir', function () { return view('respostes.afegir');})->name('view-afegir-resposta');
-Route::post('/respostes/afegir', [RespostaController::class, 'adminStore'])->name('afegir-resposta');
-Route::get('/respostes/modificar/{id}', [RespostaController::class, 'adminShow'])->name('view-modificar-resposta');
-Route::delete('/respostes/{id}', [RespostaController::class, 'adminDelete'])->name('eliminar-resposta');
-Route::patch('/respostes/modificar/{id}', [RespostaController::class, 'adminUpdate'])->name('modificar-resposta');
+Route::middleware(['auth:sanctum'])->group(function () {
+    //preguntes
+    Route::get('/preguntes', [PreguntaController::class, 'adminIndex'])->name('preguntes');
+    Route::get('/preguntes/afegir', function () { return view('preguntes.afegir');})->name('view-afegir-pregunta');
+    Route::post('/preguntes/afegir', [PreguntaController::class, 'adminStore'])->name('afegir-pregunta');
+    Route::get('/preguntes/modificar/{id}', [PreguntaController::class, 'adminShow'])->name('view-modificar-pregunta');
+    Route::patch('/preguntes/modificar/{id}', [PreguntaController::class, 'adminUpdate'])->name('modificar-pregunta');
+    Route::delete('/preguntes/{id}', [PreguntaController::class, 'adminDelete'])->name('eliminar-pregunta');
 
+
+    //respostes
+    Route::get('/respostes', [RespostaController::class, 'adminIndex'])->name('respostes');
+    Route::get('/respostes/afegir', function () { return view('respostes.afegir');})->name('view-afegir-resposta');
+    Route::post('/respostes/afegir', [RespostaController::class, 'adminStore'])->name('afegir-resposta');
+    Route::get('/respostes/modificar/{id}', [RespostaController::class, 'adminShow'])->name('view-modificar-resposta');
+    Route::delete('/respostes/{id}', [RespostaController::class, 'adminDelete'])->name('eliminar-resposta');
+    Route::patch('/respostes/modificar/{id}', [RespostaController::class, 'adminUpdate'])->name('modificar-resposta');
+});
