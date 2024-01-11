c4de8f53a94046aa2fec3d33da66cfb75c975fb5
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-01-11 13:15:44 +0100

    Update width of cards in partida.vue

--- vue-node/src/components/partida.vue
+++ vue-node/src/components/partida.vue
@@ -522,6 +522,7 @@ main {
     display: flex;
     gap: 10px;
     overflow: auto;
+    width: 90%;
 }
 
 .card {
@@ -543,7 +544,7 @@ main {
 
 .card-mobile {
     font-size: 8px;
-    width: 75px;
+    width: 100%;
     height: 100px;
     border: 3px solid white;
     border-radius: 5px;
