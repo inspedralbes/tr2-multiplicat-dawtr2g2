935a692796a8fe1cea3111cfe1b26a1c1f8ddb43
a22rauespgom <a22rauespgom@inspedralbes.cat>
2024-01-11 15:24:03 +0100

    Add console log for player info

--- Node/server.js
+++ Node/server.js
@@ -316,6 +316,7 @@ io.on("connection", (socket) => {
 
 
   socket.on("disconnect", (playerInfo) => {
+    console.log("Player info: ",playerInfo);
     players = players.filter(player => player.id !== playerInfo.id);
     io.emit("viewPlayers", players);
     console.log("Client desconectat");
