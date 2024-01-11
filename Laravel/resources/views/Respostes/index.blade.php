fe30907e854d9fea34be909115c94adee232431b
= <=>
2024-01-11 10:44:48 +0100

    Refactor button styles and add list headers

--- Laravel/resources/views/Respostes/index.blade.php
+++ Laravel/resources/views/Respostes/index.blade.php
@@ -7,16 +7,16 @@
     <h6 class="notification is-success is-light">{{ session('success') }}</h6>
     @endif
 
-    <form class="p-4 mb-4" action="{{ route('view-afegir-resposta') }}" method="POST">
+    <form class="btn-container" action="{{ route('view-afegir-resposta') }}" method="POST">
         @method('GET')
         @csrf
-        <button class="button button--icon is-success is-rounded is-responsive">
+        <button class="add-btn">
             <p>Afegir Resposta</p>
         </button>
     </form>
 
     <table class="table is-striped is-hoverable is-fullwidth">
-        <tr>
+        <tr class="list-header">
             <th>ID</th>
             <th>Resposta</th>
             <th>dificultat_id</th>
@@ -33,11 +33,87 @@
                 <form action="{{ route('eliminar-resposta', ['id' => $resposta->id]) }}" method="POST">
                     @method('DELETE')
                     @csrf
-                    <button class="button is-danger is-small is-centered eliminar">Eliminar</button>
+                    <button class="delete-btn">Eliminar</button>
                 </form>
             </td>
         </tr>
         @endforeach
     </table>
 </div>
+
+<style>
+    * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    background: none;
+    border: none;
+}
+
+p {
+    margin: 0;
+}
+
+.contenidor {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.btn-container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.add-btn {
+    background-color: #4CAF50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-weight: bold;
+    padding: 10px 20px;
+    margin: 20px 0;
+    transition: all .2s ease-in-out;
+}
+
+.add-btn:hover {
+    background-color: #3e8a41;
+}
+
+.delete-btn{
+    background-color: #f44336;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-weight: bold;
+    padding: 10px 20px;
+    transition: all .2s ease-in-out;
+}
+
+.delete-btn:hover {
+    background-color: #c53929;
+}
+
+td {
+    vertical-align: middle;
+    text-align: center;
+}
+
+.list-header {
+    background-color: #4CAF50;
+    color: white;
+    font-weight: bold;
+    padding: 10px 20px; /* Añade un poco de espacio alrededor del texto */
+    text-align: center; /* Centra el texto horizontalmente */
+    border-bottom: 2px solid #388E3C; /* Añade una línea debajo del encabezado */
+    font-size: 1.2em; /* Aumenta el tamaño del texto */
+}
+
+
+</style>
 @endsection
